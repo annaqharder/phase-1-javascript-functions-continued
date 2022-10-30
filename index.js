a2e4dfa3fe79a320eff924a0ea67ec7b485da26f
Anna Harder <annaqharder@gmail.com>
2022-10-30 17:56:08 -0600

    commit tree 6f50262b7d58e3db3d67633151947eb30519e80d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+
+function mondayWork(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+    } else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+
+function wrapAdjective(style = '*') {
+  return function innerfunction(adjective = 'special') {
+    return `You are ${style} a hard worker${style}!`
+  }
+}
+
+
+
+
+// function wrapAdjective(adjective) {
+//     let (*) = 'a hard worker'; 
+//     let || 
+//     if (adjective === *) {
+//         return `You are ${adjective} worker!`
+//     } else if (adjective === || { 
+//         return `You are a ${adjective} dedicated programmer!`
+//     }
+// }
+
